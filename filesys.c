93d2880c9e5e8241918d5dc30bec7b582c6ab236
vinnie <vrg6@zips.uakron.edu>
2021-04-28 17:35:40 -0400

    get M working

--- filesys.c
+++ filesys.c
@@ -37,22 +37,27 @@ void helpScreen()
 	printf("Format: ./yourfile.out L\n\n");
 
 	printf("D: Deletes files\n");
-	printf("Format: ./yourfile.out D filename.extension\n\n");
-	
+	printf("Format: ./yourfile.out D filename\n");
+	printf("DO NOT ADD EXTENSION TO FILENAME\n\n");
+
 	printf("P: Prints files\n");
 	printf("Format: ./yourfile.out P filename\n");
 	printf("DO NOT ADD EXTENSION TO FILENAME\n\n");
 		
 	printf("M: Creates and stores file\n");
-	
+	printf("Format: ./yourfile.out M filename\n");
+	printf("DO NOT ADD EXTENSION TO FILENAME\n\n");
+
 	return;
 }
 
 int main(int argc, char* argv[])
 {
 	// conditional for covering minimal and maximal amount of arguments for each command
-	if(((*argv[1] == 'L' && argc != 2)
-		|| (*argv[1] == 'P' && argc != 3)))
+	if((*argv[1] == 'L' && argc != 2)
+		|| (*argv[1] == 'P' && argc != 3)
+		|| (*argv[1] == 'M' && argc != 3)
+		|| (*argv[1] == 'Z' && argc != 3))
 	{
 		printf("Invalid argument number, check accepted argument formats for proper command calling\n");
 		helpScreen();
@@ -260,6 +265,283 @@ int main(int argc, char* argv[])
 
 
 		}
+		else if(*argv[1] == 'M')
+		{
+
+			int argumentSize = 0;
+			int j = 0;
+			
+			int fileNameSize = 0;
+			int foundFlag = 0;
+
+			int foundIndex = 0;
+			// get size of our input filename 
+			while(argv[2][j] != '\0')
+			{	
+				argumentSize++; 
+				j++;	
+			}	
+
+			// go through directory trying to match file name without extension
+			for(int i = 0; i < 512; i += 16)
+			{
+			
+				// stop when we hit our first zero, because everything is allocated contiguously 
+				// also stop if we found where the word was 
+				if(dir[i] == 0 || foundFlag == 1) break;
+				// go through each non null terminated letter of our input arg
+					
+				j = 0; // counting variable for going through argv 
+				fileNameSize = 0;	
+					
+				// check size of current name 
+				for(int k = 0; k < 8; k++)
+				{
+					if(dir[i+k] != 0)
+					{
+						fileNameSize++;
+					}
+					else 
+					{
+						break;
+					}	
+				}
+				// if the names are the same size
+				if(argumentSize == fileNameSize)
+				{
+					for(int z = 0; z < fileNameSize; z++)
+					{
+						if(dir[i+z] != argv[2][z])
+						{
+							foundFlag = 0; 
+							foundIndex = 0;
+							break;	
+						}
+						else 
+						{
+							foundIndex = i;
+							foundFlag = 1;
+						}
+					}
+				}
+
+				if(foundFlag == 1)
+				{
+					break;
+				}
+			}
+			// we found out the file already exists 
+			if(foundFlag == 1) 
+			{
+				printf("Error: file name already exists...\n");
+			}
+			// the file does not exist, so we can begin writing
+			else
+			{
+				// TODO: prompt user for string 
+				int freeDirectorySpot = 0;
+				int foundFreeSpot = 0;
+
+				// find free spot to put name of new file in directory				
+				for(int p = 0; p < 512; p += 16)
+				{
+
+					// move in 16 bit chunks 
+					if(dir[p] == 0)
+					{
+						freeDirectorySpot = p; 
+						foundFreeSpot = 1;
+						break;
+					}
+				}
+				
+				// if we cound't find a free spot, exit program 
+				if(foundFreeSpot == 0)
+				{
+					printf("Not enough space!\n");
+					return 0;
+				}
+
+				// put name into new free space 
+				int padCount = 0;
+				
+				// copy name down 
+				while(padCount < 8)
+				{
+					if(argv[2][padCount] == '\0') // if we hit a null terminator, break away and start padding with zeroes
+					{
+						break;
+					}
+					else 
+					{
+						dir[freeDirectorySpot + padCount] = argv[2][padCount];
+						padCount++;
+					}
+				}
+				// if we have space remaining
+				if(padCount != 7)
+				{
+					while(padCount < 8)
+					{
+						dir[freeDirectorySpot + padCount] = 0;
+						padCount++;
+					}
+				}
+
+				// append 't' to the 9th spot to indicate this is a text file 
+				dir[freeDirectorySpot + 8] = 't';
+
+				// apend '1' for sector lenght, because we KNOW our text file will only be about 512b at most 
+				dir[freeDirectorySpot + 10] = 1;
+
+				// search for a free spot in the map 
+				int foundMapFreeSpot = 0; // boolean variable for finding map spot 
+				int mapFreeSpot = 0; // location of free spot in map
+				
+				for(int i = 0; i < 512; i++)
+				{
+					if(map[i] == 0)
+					{
+						mapFreeSpot = i;
+						foundMapFreeSpot = 1;
+						map[mapFreeSpot] = 255; // set the entry in map to 255
+						break;
+					}
+				}
+				
+				if(foundMapFreeSpot == 0)
+				{
+					printf("Did not find free spot in map...\n");
+					return 0;
+				}
+
+				// write free spot in directory
+				dir[freeDirectorySpot + 9] = mapFreeSpot;
+
+
+				// prompt user for a string 
+				printf("Enter a message to store: ");
+				
+				char msg[512];
+				fgets(msg, sizeof(msg), stdin);
+				
+				// since we know the file is in the directory
+				// we must get the start sector
+				int startSector = dir[freeDirectorySpot + 9];
+
+				fseek(floppy, startSector*512, SEEK_SET); // locate sector where file is
+				for (i=0; i<512; i++) fputc(msg[i],floppy); // write to map 
+
+				int sectorLength = dir[freeDirectorySpot + 10];
+			}
+
+
+		}
+		else if(*argv[1] == 'Z')
+		{
+			//print disk map
+			printf("Disk usage map:\n");
+			printf("      0 1 2 3 4 5 6 7 8 9 A B C D E F\n");
+			printf("     --------------------------------\n");
+			for (i=0; i<16; i++) {
+				switch(i) {
+					case 15: printf("0xF_ "); break;
+					case 14: printf("0xE_ "); break;
+					case 13: printf("0xD_ "); break;
+					case 12: printf("0xC_ "); break;
+					case 11: printf("0xB_ "); break;
+					case 10: printf("0xA_ "); break;
+					default: printf("0x%d_ ", i); break;
+				}
+				for (j=0; j<16; j++) {
+					if (map[16*i+j]==-1) printf(" X"); else printf(" .");
+				}
+				printf("\n");
+			}
+
+    		// print directory
+			printf("\nDisk directory:\n");
+			printf("Name    Type Start Length\n");
+    		for (i=0; i<512; i=i+16) {
+				if (dir[i]==0) break;
+				for (j=0; j<8; j++) {
+					if (dir[i+j]==0) printf(" "); else printf("%c",dir[i+j]);
+				}
+				if ((dir[i+8]=='t') || (dir[i+8]=='T')) printf("text"); else printf("exec");
+				printf(" %5d %6d bytes\n", dir[i+9], 512*dir[i+10]);
+			}
+		}
+		else if(*argv[1] == 'D')
+		{
+			
+
+			int argumentSize = 0;
+			int j = 0;
+			
+			int fileNameSize = 0;
+			int foundFlag = 0;
+
+			int foundIndex = 0;
+			// get size of our input filename 
+			while(argv[2][j] != '\0')
+			{	
+				argumentSize++; 
+				j++;	
+			}	
+
+			// go through directory trying to match file name without extension
+			for(int i = 0; i < 512; i += 16)
+			{
+			
+				// stop when we hit our first zero, because everything is allocated contiguously 
+				// also stop if we found where the word was 
+				if(dir[i] == 0 || foundFlag == 1) break;
+				// go through each non null terminated letter of our input arg
+					
+				j = 0; // counting variable for going through argv 
+				fileNameSize = 0;	
+					
+				// check size of current name 
+				for(int k = 0; k < 8; k++)
+				{
+					if(dir[i+k] != 0)
+					{
+						fileNameSize++;
+					}
+					else 
+					{
+						break;
+					}	
+				}
+				// if the names are the same size
+				if(argumentSize == fileNameSize)
+				{
+					for(int z = 0; z < fileNameSize; z++)
+					{
+						if(dir[i+z] != argv[2][z])
+						{
+							foundFlag = 0; 
+							foundIndex = 0;
+							break;	
+						}
+						else 
+						{
+							foundIndex = i;
+							foundFlag = 1;
+						}
+					}
+				}
+
+				if(foundFlag == 1)
+				{
+					break;
+				}
+
+
+
+
+
+		}
 		else 
 		{
 			printf("Invalid argument, here is a list of valid arguments...\n");
@@ -267,15 +549,15 @@ int main(int argc, char* argv[])
 		}
 
 		
-		//write the map and directory back to the floppy image
+		//write map back 
 		fseek(floppy,512*256,SEEK_SET);
 		for (i=0; i<512; i++) fputc(map[i],floppy);
 
+		//write dir back
 		fseek(floppy,512*257,SEEK_SET);
 		for (i=0; i<512; i++) fputc(dir[i],floppy);
 		
 
-		//printf("%s", argv[1]);
 		fclose(floppy);
 	
 	}
